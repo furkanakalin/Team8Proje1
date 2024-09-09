08ba8f6049d1a34b124daa56aac85cc6fc224f01
Abylmyrat Gummanov <abdy.coding@gmail.com>
2024-09-09 22:06:43 +0300

    Changes updated

--- src/Team8/TC07/TC07.java
+++ src/Team8/TC07/TC07.java
@@ -1,4 +1,16 @@
 package Team8.TC07;
 
-public class TC07 {
+import Utility.BaseDriver;
+import org.junit.Test;
+
+
+public class TC07  extends BaseDriver {
+    @Test
+    public void Test(){
+        driver.get("https://www.akakce.com/");
+
+
+
+
+    }
 }

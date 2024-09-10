acc4036074797f1bc64cdb9b23e25809901f517b
yaseyldrr <yase.yldrr@gmail.com>
2024-09-10 16:39:20 +0300

    User Story-0404 code is created.

---
+++ src/Team8/TC04/TC04_04.java
@@ -0,0 +1,34 @@
+package Team8.TC04;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TC04_04 extends BaseDriver {
+    @Test
+    public void Test1() {
+        driver.get("https://www.akakce.com/");
+        MyFunc.Bekle(2);
+
+        WebElement loginElement = driver.findElement(By.xpath("//a[@href='/akakcem/giris/']"));
+        loginElement.click();
+        MyFunc.Bekle(2);
+
+        WebElement email = driver.findElement(By.xpath("//input[@id='life']"));
+        email.sendKeys("");
+
+        WebElement password = driver.findElement(By.xpath("//input[@id='lifp']"));
+        password.sendKeys("");
+
+        WebElement submit = driver.findElement(By.xpath("//input[@id='lfb']"));
+        submit.click();
+        MyFunc.Bekle(2);
+
+        WebElement controlMessage = driver.findElement(By.cssSelector(".alertX > p:nth-child(1)"));
+        Assert.assertEquals("Failed","Lütfen e-posta adresinizi yazın.",controlMessage.getText());
+        BekleKapat();
+    }
+}

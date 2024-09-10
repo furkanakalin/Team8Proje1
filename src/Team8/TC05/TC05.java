eb1138f8ec6355870cb547320b1d635e7ee4a0bd
mhroz <mhr.ozc@hotmail.com>
2024-09-10 20:53:28 +0300

    10.09 20:50

--- src/Team8/TC05/TC05.java
+++ src/Team8/TC05/TC05.java
@@ -1,5 +1,6 @@
 package Team8.TC05;
 
+import Utility.MyFunc;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,25 +11,23 @@ public class TC05 {
         WebDriver driver = new ChromeDriver();
         driver.get("https://akakce.com");
         driver.manage().window().maximize();
+        MyFunc.Bekle(1);
 
         WebElement girisYapButton = driver.findElement(By.linkText("Giriş Yap"));
         girisYapButton.click();
+        MyFunc.Bekle(1);
 
         WebElement emailGiris= driver.findElement(By.id("life"));
         emailGiris.sendKeys("technostudyteam8@gmail.com");
 
         WebElement sifreGiris = driver.findElement(By.id("lifp"));
         sifreGiris.sendKeys("Furkan24");
+        MyFunc.Bekle(1);
 
         WebElement loginButton = driver.findElement(By.id("lfb"));
         loginButton.click();
 
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
+        MyFunc.Bekle(1);
         driver.quit();
     }
 }
